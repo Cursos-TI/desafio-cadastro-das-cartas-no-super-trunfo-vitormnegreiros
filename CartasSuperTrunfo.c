317c1c960ca8d90133d7f902ac5b812225ca35b1
vitormnegreiros <145074220+vitormnegreiros@users.noreply.github.com>
2025-08-31 01:38:23 +0000

    adição das medias

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+float calculadensidade(int populacao, float area){
+    return populacao/area;
+};
+float calculapib(float pib,int populacao){
+    return pib/populacao;
+};
+
 int main() {
  char estadoA, estadoB;
  char codigocartaA[3], codigocartaB[3];
@@ -8,7 +15,7 @@ int main() {
  float areaA, areaB;
  float pibA, pibB;
  int pontoturisticoA, pontoturisticoB;
- 
+
  printf("digite a primeira letra do estado A:\n");
  scanf(" %c", &estadoA);
  
@@ -16,16 +23,16 @@ int main() {
  scanf(" %c", &estadoB);
  
  printf("digite o codigo da carta A:\n");
- scanf("%s", codigocartaA);
+ scanf("%3[^\n]", codigocartaA);
  
  printf("digite o codigo da carta B:\n");
- scanf("%s", codigocartaB);
+ scanf("%3[^\n]", codigocartaB);
  
  printf("digite o nome da cidade A:\n");
- scanf(" %[^\n]", nomecidadeA);
+ scanf(" %19[^\n]", nomecidadeA);
  
  printf("digite o nome da cidade B:\n");
- scanf(" %[^\n]", nomecidadeB);
+ scanf(" %19[^\n]", nomecidadeB);
  
  printf("digite o numero de habitantes da cidade A:\n");
  scanf("%d", &populacaoA);
@@ -51,16 +58,26 @@ int main() {
  printf("digite a quantidade de pontos turisticos da cidade B:\n");
  scanf("%d", &pontoturisticoB);
 
+ float densidadecA = calculadensidade(populacaoA,areaA);
+ float densidadecB = calculadensidade(populacaoB,areaB);
 
+ float pibcA = calculapib(pibA,populacaoA);
+ float pibcB = calculapib(pibB,populacaoB);
 
-printf("\n--- Carta A ---\n");
-printf("Estado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nPontos turísticos: %d\n",
+ printf("\n--- Carta A ---\n");
+ printf("Estado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nPontos turísticos: %d\n",
     estadoA, codigocartaA, nomecidadeA, populacaoA, areaA, pibA, pontoturisticoA);
+ printf("a densidade da carta A é: %f\n", densidadecA );
+ printf(" O resultado do PIB per capita da carta A é: %f\n", pibcA );
 
-printf("\n--- Carta B ---\n");
-printf("Estado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nPontos turísticos: %d\n",
+printf("\n \n");
+
+ printf("\n--- Carta B ---\n");
+ printf("Estado: %c\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nPontos turísticos: %d\n",
     estadoB, codigocartaB, nomecidadeB, populacaoB, areaB, pibB, pontoturisticoB);
 
+ printf("a densidade da carta B é: %f\n", densidadecB );
+ printf(" O resultado do PIB per capita da carta B é: %f\n", pibcB );
 
     return 0;
 }
